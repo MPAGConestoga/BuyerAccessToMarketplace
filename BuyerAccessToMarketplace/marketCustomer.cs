7dea56a8b2089ac55c279dea46022d74ba12c944
Amy Dayasundara <a.dayasundara@gmail.com>
2019-11-16 11:37:51 -0500

    customerclass?

--- BuyerAccessToMarketplace/marketCustomer.cs
+++ BuyerAccessToMarketplace/marketCustomer.cs
@@ -8,8 +8,58 @@ namespace BuyerAccessToMarketplace
 {
     public class marketCustomer
     {
-        private string ClientName;
-        private string JobType;
+        private string clientName;
+        private int jobType;
+        private int quantity;
+        private string origin;
+        private string destination;
+        private int vanType;
 
+        public marketCustomer(string clientName, string jobType, string quantity, string origin, string destination, string vanType)
+        {
+            this.clientName = clientName;
+            try
+            {
+                this.jobType = System.Convert.ToInt32(jobType);
+            }
+            catch(OverflowException)
+            {
+
+            }
+            catch(FormatException)
+            {
+
+            }
+
+            try
+            {
+                this.jobType = System.Convert.ToInt32(quantity);
+            }
+            catch (OverflowException)
+            {
+
+            }
+            catch (FormatException)
+            {
+
+            }
+
+            this.origin = origin;
+            this.destination = destination;
+
+            try
+            {
+                this.vanType = System.Convert.ToInt32(vanType);
+            }
+            catch(OverflowException)
+            {
+               
+            }
+            catch(FormatException)
+            {
+
+            }
+
+        }
     }
 }
